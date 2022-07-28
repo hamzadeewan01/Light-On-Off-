e81052f6b1e3d3abc951384772873479f9fb343f
Muhammad Hamza Deewan <102538357+hamzadeewan01@users.noreply.github.com>
2022-07-28 21:13:43 +0500

    done

---
+++ app.js
@@ -0,0 +1,9 @@
+function offbulb(){
+    var img = document.getElementById("img");
+    img.src = "./download 2.jpg"
+
+}
+function onbulb(){
+    var img = document.getElementById("img");
+    img.src = "./download 1.jpg";
+}
